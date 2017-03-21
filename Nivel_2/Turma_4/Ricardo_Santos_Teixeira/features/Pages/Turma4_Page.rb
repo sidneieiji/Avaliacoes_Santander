91e75e75fbd70e3a460a1358fadd399982bb2c9a
Sidnei Eiji Nishiyama <nishiyama@MacBook-Pro-de-Inmetrics.local>
2017-03-21 10:39:46 -0300

    Atualizacao Pasta do Ricardo Teixeira - Turma 4

--- Nivel_2/Turma_4/Ricardo_Santos_Teixeira/features/Pages/Turma4_Page.rb
+++
@@ -1,9 +0,0 @@
-class Home < SitePrism::Page
-  set_url "https://vtrmartinez.wixsite.com/dojo"
-  	element :menu_contato, "#DrpDwnMn02label"
-	element :contato, "#CntctFrm1nameField"
-	element :email, "#CntctFrm1emailField"
-	element :assunto, "#CntctFrm1subjectField"
-	element :mensagem, "#CntctFrm1messageField"
-    element :botao_enviar, "#CntctFrm1submit"
-end
